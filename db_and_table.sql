714cfaeb00a84533643c479eed88e83a9b487bcf
Roman Banakh <banakh.ri@gmail.com>
2019-08-30 18:33:08 +0300

    Added db schema

---
+++ db_and_table.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE cfn_workshop;
+CREATE TABLE cfn_workshop.user (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP NOT NULL
+);
